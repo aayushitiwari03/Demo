b7ee23a4c11b8ea0c7e631142bea75eacac8208f
Aayushi <aayushitiwari21@navgurukul.org>
2024-02-02 22:17:24 +0530

    added round Image on profile screen

--- app/src/main/java/com/aayushi/demo/screen/bottomNav/ProfileScreen.kt
+++ app/src/main/java/com/aayushi/demo/screen/bottomNav/ProfileScreen.kt
@@ -1,19 +1,50 @@
 package com.aayushi.demo.screen.bottomNav
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.aayushi.demo.R
+import com.aayushi.demo.ui.theme.green
 
 @Composable
 fun ProfileScreen() {
-
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "Profile Screen", fontSize = 20.sp)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            // Text information on the left
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Atsushi", fontSize = 16.sp)
+                Text(text = "Atsushi@gmail.com", fontSize = 16.sp)
+            }
+            Image(
+                painter = painterResource(id = R.drawable.img_6),
+                contentDescription = "Big Elephant",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(45.dp)
+                    .clip(CircleShape)
+
+            )
+        }
     }
 
 }
