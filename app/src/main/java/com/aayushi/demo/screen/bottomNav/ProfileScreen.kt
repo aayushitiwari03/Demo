d43769b12e3ca22bb1271b190b9bb9525486fbf9
Aayushi <aayushitiwari21@navgurukul.org>
2024-02-03 00:23:51 +0530

    profile screen

--- app/src/main/java/com/aayushi/demo/screen/bottomNav/ProfileScreen.kt
+++ app/src/main/java/com/aayushi/demo/screen/bottomNav/ProfileScreen.kt
@@ -1,11 +1,16 @@
 package com.aayushi.demo.screen.bottomNav
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -14,7 +19,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aayushi.demo.R
-import com.aayushi.demo.ui.theme.green
+
 
 @Composable
 fun ProfileScreen() {
@@ -45,6 +50,127 @@ fun ProfileScreen() {
 
             )
         }
+
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color.LightGray)
+                    .padding(start = 16.dp, end = 16.dp)
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Following",
+                style = MaterialTheme.typography.body1,
+            )
+
+            Text(
+                text = "See All",
+                style = MaterialTheme.typography.body1,
+
+            )
+        }
+
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+
+        ) {
+            items(2) {
+                Image(
+                    painter = painterResource(id = R.drawable.img_7),
+                    contentDescription = "Following",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clip(CircleShape)
+                        .padding(2.dp)
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color.LightGray)
+                    .padding(start = 16.dp, end = 16.dp)
+            )
+        }
+
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            item {
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp)
+                        .background(Color.LightGray)
+                        .padding(start = 16.dp, end = 16.dp)
+                )
+            }
+
+            items(7) { index ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Image(
+                        painter = painterResource(id = R.drawable.img_7),
+                        contentDescription = "Image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(45.dp)
+                            .clip(CircleShape)
+                    )
+
+
+                    Text(
+                        text = "Item $index",
+                        style = MaterialTheme.typography.body1,
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                            .weight(1f)
+                    )
+
+
+                    Image(
+                        painter = painterResource(id = R.drawable.baseline_arrow_forward_ios_24),
+                        contentDescription = "Drawable",
+                        modifier = Modifier
+                            .size(24.dp)
+                            .padding(end = 16.dp)
+                    )
+                }
+
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+            }
+        }
+
+
+
     }
 
 }
