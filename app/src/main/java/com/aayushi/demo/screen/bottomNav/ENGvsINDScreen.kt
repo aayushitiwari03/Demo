21f2aa56ed2be1fd3e00dc6f3e422d1c18923815
Aayushi <aayushitiwari21@navgurukul.org>
2024-02-03 12:47:50 +0530

    design a screen

--- app/src/main/java/com/aayushi/demo/screen/bottomNav/ENGvsINDScreen.kt
+++ app/src/main/java/com/aayushi/demo/screen/bottomNav/ENGvsINDScreen.kt
@@ -1,11 +1,18 @@
 package com.aayushi.demo.screen.bottomNav
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.aayushi.demo.R
 
 @Composable
 fun ENGvsIND() {
@@ -13,7 +20,17 @@ fun ENGvsIND() {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "Report Screen", fontSize = 20.sp)
+        LazyRow(
+            modifier = Modifier.padding(8.dp)
+        )
+        {
+            items(10) {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription =null, modifier = Modifier.size(100.dp)
+                )
+            }
+        }
     }
 
 }
